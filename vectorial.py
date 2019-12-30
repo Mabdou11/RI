f317c215029647cf7225d745a95156e77ba31e24
Mabdou11 <menouerabdou@gmail.com>
2019-12-30 05:47:34 +0100

    weighted rev file (not created)

--- vectorial.py
+++ vectorial.py
@@ -1,7 +1,6 @@
 import re
 from nltk.corpus import stopwords
 from nltk import FreqDist as fd
-import math
 # step 1:
 
 documents = open("./cacm/cacm.all","r+", encoding="utf-8").read()
@@ -12,14 +11,6 @@ def get_doc(number):
 	if not match:
 		return ""
 	return match.group(0)
-
-def num_docs():
-	i=1
-	while(get_doc(i)!=""):
-		i+=1
-	return i-1
-
-N = num_docs()
 
 def get_title(number):
 	doc = get_doc(number)
@@ -55,75 +46,16 @@ def freq_word_all(word):
 		i+=1
 	return sum
 
-def docs_contain(word):
-	i = 1
-	docs =[]
-	while(i<=N):
-		if word in [words[0] for words in fdWords(i).items()]:
-			docs.append(i)
-		i+=1
-	return docs
-
-
 def wdf():#word_doc_freq
 	sum = 0
-	rev = []
 	i = 1
+	rev = []
 	while(get_doc(i)!=""):
 		for w in fdWords(i).items():
 			rev.append((w[0],i,w[1]))
 		i+=1
 	#rev = sorted(rev, key=lambda tup: tup[0])
 	return rev
-
-def wdf_dict():#word_doc_freq
-	sum = 0
-	rev = []
-	i = 1
-	_dict = {}
-	while(get_doc(i)!=""):
-		for w in fdWords(i).items():
-			_dict["doc"] = i
-			_dict["word"] = w[0]
-			_dict["freq"] = w[1]
-
-			rev.append(_dict)
-		i+=1
-	#rev = sorted(rev, key=lambda tup: tup[0])
-	return rev
-
-
-def weighted_wdf():#word_doc_freq
-	#fornmula
-	
-	N = 1
-	i = 1
-	rev = []
-	#poids(ti, dj)=(freq(ti,dj)/Max(freq(dj))*math.log((N/ni) +1)
-
-	while(get_doc(N)!=""):
- 		N+=1
-	N-=1
-	weighted= open("weighted_reversed_file.txt","w+",  encoding="utf-8")	
-	while(get_doc(i)!=""):
-		doc = fdWords(i).items()
-		maxF =  max([word[1] for word in doc])
-		for word in doc:
-			ni = len(docs_contain(word[0]))
-			freq = word[1]
-			weight = (freq/maxF)*(math.log10(N/ni+1)+1)
-			weighted.write("("+word[0]+", "+str(i)+") -> "+str(weight)+"\n")
-			print("("+word[0]+", "+str(i)+") -> "+str(weight)+"\n")
-			#rev.append((word[0],i,weight))
-			print(""+str((word[0],i,weight)))			
-		i+=1
-	#rev = sorted(rev, key=lambda tup: tup[0])
-	return print("created weighted reversed file") #rev
-
-
-##TODO: this will take forever
-print(weighted_wdf())
-
 
 
 def create_indexFile():
@@ -140,46 +72,15 @@ def create_indexFile():
 	return "index file created"
 
 
-"""print("----step1.2----")
-print (fdWords(123).items())
-print (freq_word('compiler',123))
-print("\n------------all--------------\n")
-print(get_words(123))
-print("\n--------------------------\n")
-"""
 
 def create_revFile():
 	with open("reversed_file.txt","w+",  encoding="utf-8")as w:
 		wdf()[0]
 		for tup in wdf():
 			w.write("("+tup[0]+", "+str(tup[1])+") -> "+str(tup[2])+"\n")
-			print("("+tup[0]+", "+str(tup[1])+") -> "+str(tup[2])+"\n")
 		w.close()
 	return "reversed file created"
 
-
-def create_weighted_revFile():
-	weighted= open("weighted_reversed_file.txt","w+",  encoding="utf-8")
-	for tup in weighted_wdf():
-		weighted.write("("+tup[0]+", "+str(tup[1])+") -> "+str(tup[2])+"\n")
-	weighted.close
-	return "created weighted reversed file"
-
-print(create_weighted_revFile())
-
-def booleanModel_for_rev(terms):
-
-	terms = terms.split()
-	r= open("index_file.txt","r+",  encoding="utf-8")
-	doc = r.readlines()
-	tdf = []
-	for line in doc:
-		match = re.search(r"\((.*?), (.*?)\) -> (.*)", line)
-
-		term, doc, freq = match.group(1),match.group(2),match.group(3)
-		tdf.append((term, doc, freq))
-	r.close
-	return 0
 
 
 def booleanModel(terms):
@@ -203,14 +104,16 @@ def booleanModel(terms):
 				word = re.search(r"\((.*?), ([0-9]+)\)", doc[i])
 				temp_doc.append(word.group(1))
 				i+=1
+				
 			if(any(elem in temp_doc for elem in and_terms)):
 				pertinent_docs.append(d)
 	r.close
 	return pertinent_docs
 
 
-
 #print(create_indexFile())
-#print(booleanModel('algebraic'))
+
+
+print(booleanModel('algebraic'))
 
 
